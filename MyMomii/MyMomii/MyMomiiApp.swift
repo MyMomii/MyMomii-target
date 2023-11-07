6303add86e165665863c5f704ca531dbd65e6a59
jaelyung <jaekim7724@gmail.com>
2023-11-07 13:58:23 +0900

    [chore] 프로젝트 최초 세팅

---
+++ MyMomii/MyMomii/MyMomiiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMomiiApp.swift
+//  MyMomii
+//
+//  Created by jaelyung kim on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MyMomiiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
