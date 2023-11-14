5ee3c5acd7925413c33d73f661c948413f6adf40
zyeongPark <piiy10141997@gmail.com>
2023-11-14 12:13:59 +0900

    [feat] 런치스크린까지 추가 완료

--- MyMomii/MyMomii/UI/OnboardingViews/LaunchScreenView.swift
+++ MyMomii/MyMomii/UI/OnboardingViews/LaunchScreenView.swift
@@ -8,21 +8,45 @@
 import SwiftUI
 
 struct LaunchScreenView: View {
+    @State private var isActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.3
+    
     var body: some View {
-        VStack(alignment: .center, spacing: 20) {
-            Image("AppTitle")
-                .resizable()
-                .frame(width: 126, height: 55)
-            Image("SMOnboarding")
-                .resizable()
-                .frame(width: 130, height: 214)
-                .shadow(color: .black.opacity(0.15), radius: 5, x: 0, y: 4)
-            Text("마이모미와 함께 \n소중한 월경 주기를 관리해보아요")
-                .semiBold16White50()
-                .multilineTextAlignment(.center)
+        if isActive {
+            TargetMainView()
+        } else {
+            VStack(alignment: .center, spacing: 20) {
+                Image("AppTitle")
+                    .resizable()
+                    .frame(width: 126, height: 55)
+                Image("SMOnboarding")
+                    .resizable()
+                    .frame(width: 130, height: 214)
+                    .shadow(color: .black.opacity(0.15), radius: 5, x: 0, y: 4)
+                Text("마이모미와 함께 \n소중한 월경 주기를 관리해보아요")
+                    .semiBold16White50()
+                    .multilineTextAlignment(.center)
+            }
+            .scaleEffect(size)
+            .opacity(opacity)
+            .onAppear {
+                withAnimation(.easeIn(duration: 1.2)) {
+                    self.size = 1.2
+                    self.opacity = 1.0
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.coral200)
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.coral200)
+
     }
 }
 
