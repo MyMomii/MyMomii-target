5fab2fbdb362ad4f3bbe81f59e77901ee57d76ae
jaelyung <jaekim7724@gmail.com>
2023-11-14 21:27:18 +0900

    [feat] SympView에서 증상을 입력하고 firestore에 저장하는 모델 파일 생성 - 현재 접속한 user(anonymous)가 누구인지 확인하는 기능 - 생리주기 및 증상을 입력할 수 있는 기능 - 생리주기 및 증상을 제거할 수 있는 기능

--- MyMomii/MyMomii/UI/TargetViews/SympViewModel.swift
+++ MyMomii/MyMomii/UI/TargetViews/SympViewModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import SwiftUI
+
+@MainActor
+final class SympViewModel: ObservableObject {
+
+    @Published private(set) var user: DBUser? = nil
+
+    func loadCurrentUser() async throws {
+        let authDataResult = try AuthenticationManager.shared.getAuthenticatedUser()
+        self.user = try await UserManager.shared.getUser(userId: authDataResult.uid)
+    }
+
+    func addMensInfo(mensSymp: String, mensAmt: String, emoLv: String, dateOfMens: Date) {
+        guard let user else { return }
+        let mensInfo = MensInfo(id: Date(), mensSymp: mensSymp, mensAmt: mensAmt, emoLv: emoLv, dateOfMens: dateOfMens, regDt: Date())
+        Task {
+            try await UserManager.shared.addMensInfo(userId: user.userId, mensInfo: mensInfo)
+        }
+    }
+
+    func removeMensInfo() {
+        guard let user else { return }
+        Task {
+            try await UserManager.shared.removeFavoriteMovie(userId: user.userId)
+            self.user = try await UserManager.shared.getUser(userId: user.userId)
+        }
+    }
+}
