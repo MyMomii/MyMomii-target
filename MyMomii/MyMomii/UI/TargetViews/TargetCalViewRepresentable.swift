c94449cfb0cd0e002c26562a18f5823f0db9be6b
oceanooooo <funboard6287@gmail.com>
2023-11-15 01:06:35 +0900

    [feat] 캘린더 UI 구현

---
+++ MyMomii/MyMomii/UI/TargetViews/TargetCalViewRepresentable.swift
@@ -0,0 +1,18 @@
+//
+//  TargetCalViewRepresentable.swift
+//  MyMomii
+//
+//  Created by OhSuhyun on 2023.11.14.
+//
+
+import SwiftUI
+
+struct TargetCalViewRepresentable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TargetCalViewRepresentable()
+}
