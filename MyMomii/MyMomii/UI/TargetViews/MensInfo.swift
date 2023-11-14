1847f8e527c919bfc83c571b1d8cdd667084b51a
jaelyung <jaekim7724@gmail.com>
2023-11-14 18:16:10 +0900

    [feat] 온보딩뷰에서 '시작하기'버튼 클릭시 익명로그인 되는 기능 추가

---
+++ MyMomii/MyMomii/UI/TargetViews/MensInfo.swift
@@ -0,0 +1,8 @@
+//
+//  MensInfo.swift
+//  MyMomii
+//
+//  Created by jaelyung kim on 11/14/23.
+//
+
+import Foundation
---
+++ MyMomii/MyMomii/UI/TargetViews/SympViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SympViewModel.swift
+//  MyMomii
+//
+//  Created by jaelyung kim on 11/14/23.
+//
+
+import Foundation
