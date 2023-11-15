55c53bfc90d7e25a0f04d82c2be3582c749d5af8
jaelyung <jaekim7724@gmail.com>
2023-11-15 23:22:54 +0900

    [feat] 달력에서 선택된 날짜에 저장된 데이터 값 불러오는 기능 추가

--- MyMomii/MyMomii/UI/TargetViews/TargetCalViewModel.swift
+++ MyMomii/MyMomii/UI/TargetViews/TargetCalViewModel.swift
@@ -11,9 +11,15 @@ import SwiftUI
 final class TargetCalViewModel: ObservableObject {
 
     @Published private(set) var mensInfos: [MensInfo] = []
+    @Published private(set) var mensInfosForSelectedDate: [MensInfo] = []
 
     func getAllMensInfos() async throws {
         let authDataResult = try AuthenticationManager.shared.getAuthenticatedUser()
         self.mensInfos = try await UserManager.shared.getAllMensInfo(userId: authDataResult.uid)
+    }
+
+    func getMensInfoForSelectedDate(selectedDate: String) async throws {
+        let authDataResult = try AuthenticationManager.shared.getAuthenticatedUser()
+        self.mensInfosForSelectedDate = try await UserManager.shared.getMensInfoForSelectedDate(userId: authDataResult.uid, selectedDate: selectedDate)
     }
 }
